34faf3c9f2ab871f2fdc986bd8b304c641c160f6
vidura-12 <viduranirmal195@gmail.com>
2026-04-20 20:05:50 +0530

    Update Program.cs

--- Deliveroo.Gateway/Program.cs
+++ Deliveroo.Gateway/Program.cs
@@ -9,5 +9,18 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-await app.UseOcelot();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowNetlify",
+        policy => {
+            policy.WithOrigins("https://glowing-sunshine-d907db.netlify.app") // Add your Netlify URL
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials();
+        });
+});
+
+app.UseCors("AllowNetlify"); 
+
+app.UseOcelot().Wait();
 app.Run();
